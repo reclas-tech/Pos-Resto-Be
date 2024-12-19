cfcb9781479d16c6f404a5df838e767a1f7c0804
RozKoy <okkoy.1401@gmail.com>
2024-12-20 00:27:21 +0700

    feat(helper): response

---
+++ app/Helpers/Response.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\JsonResponse;
+use App\Traits\StatusCode;
+
+class Response
+{
+	use StatusCode;
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Variables
+	|--------------------------------------------------------------------------
+	*/
+
+	private int $status = Response::OK;
+	private string $message = '';
+	private mixed $data = [];
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Functions
+	|--------------------------------------------------------------------------
+	*/
+
+	/**
+	 * @param int $status
+	 * @param string $message
+	 * @param mixed $data
+	 */
+	public function __construct(int $status = Response::OK, string $message = '', mixed $data = [])
+	{
+		$this->set($status, $message, $data);
+	}
+
+	/**
+	 * @param int $status
+	 * @param string $message
+	 * @param mixed $data
+	 * 
+	 * @return void
+	 */
+	public function set(int|null $status = null, string|null $message = null, mixed $data = null): void
+	{
+		$this->message = $message ?? $this->message;
+		$this->status = $status ?? $this->status;
+		$this->data = $data ?? $this->data;
+	}
+
+	/**
+	 * @param int $status
+	 * @param string $message
+	 * @param mixed $data
+	 * 
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function get(int|null $status = null, string|null $message = null, mixed $data = null): JsonResponse
+	{
+		$this->set($status, $message, $data);
+
+		return $this->SetAndGet($this->status, $this->message, $this->data);
+	}
+
+	/**
+	 * @param int $status
+	 * @param string $message
+	 * @param mixed $data
+	 * 
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public static function SetAndGet(int $status = Response::OK, string $message = '', mixed $data = []): JsonResponse
+	{
+		return response()->json(
+			data: [
+				'status' => $status,
+				'message' => $message,
+				'data' => $data,
+			],
+			status: $status,
+		);
+	}
+}
