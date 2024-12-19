f578c6f2d88a4fa8440b0bc9f80fa6a7bc724300
RozKoy <okkoy.1401@gmail.com>
2024-12-20 00:36:35 +0700

    feat(app bootstrap): api json response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use App\Helpers\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,15 +13,15 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__ . '/../routes/web.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (AuthenticationException $e, Request $request) {
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'message' => $e->getMessage(),
-                ], 401);
-            }
-        });
+    ->withExceptions(function (Exceptions $exceptions): void {
+        if (env('APP_ENV', 'production') === 'production' || env('APP_API_JSON_RESPONSE', false) === true) {
+            $exceptions->render(function (Throwable $e, Request $request) {
+                if ($request->is('api/*')) {
+                    return Response::SetAndGet(Response::INTERNAL_SERVER_ERROR, $e->getMessage());
+                }
+            });
+        }
     })->create();
