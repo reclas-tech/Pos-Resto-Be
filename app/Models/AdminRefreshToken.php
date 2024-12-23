6abecf8eea7fad5a816e388f4637f4ddfa399ef9
RozKoy <okkoy.1401@gmail.com>
2024-12-23 19:21:56 +0700

    feat(model n migration): admin refresh token

---
+++ app/Models/AdminRefreshToken.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminRefreshToken extends Model
+{
+    use HasUuids, SoftDeletes;
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Variables
+    |--------------------------------------------------------------------------
+    */
+
+    protected $table = 'admin_refresh_tokens';
+
+    protected $fillable = [
+        // REQUIRED
+        'expired_at',
+        'token',
+
+        // FOREIGN KEY
+        'admin_id'
+    ];
+
+    protected $hidden = [
+        'admin_id',
+        'token',
+    ];
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relations
+    |--------------------------------------------------------------------------
+    */
+
+    public function admin(): BelongsTo
+    {
+        return $this->belongsTo(Admin::class);
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Functions
+    |--------------------------------------------------------------------------
+    */
+
+}
