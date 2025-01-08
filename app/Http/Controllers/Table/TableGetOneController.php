5eec4a7d5407a3719ad6df1862f53588103e459c
randaru09 <ndaruanggoro123@gmail.com>
2025-01-08 18:38:02 +0700

    feat: get one controller for table

---
+++ app/Http/Controllers/Table/TableGetOneController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Table;
+
+use Illuminate\Http\JsonResponse;
+use App\Helpers\Response;
+
+class TableGetOneController extends BaseController
+{
+    public function action(string $id): JsonResponse
+    {
+        $table =  $this->tableService->getById($id);
+        if ($table === null) {
+            return Response::SetAndGet(Response::NOT_FOUND, 'Data meja tidak dapat ditemukan');
+        }
+        return Response::SetAndGet(message: 'Meja Berhasil Didapatkan', data: $table->toArray());
+    }
+}
