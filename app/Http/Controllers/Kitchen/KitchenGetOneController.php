12fba3f53d8b322048cd52d694761b69eacf2750
randaru09 <ndaruanggoro123@gmail.com>
2025-01-08 17:50:04 +0700

    feat: add null condition

--- app/Http/Controllers/Kitchen/KitchenGetOneController.php
+++ app/Http/Controllers/Kitchen/KitchenGetOneController.php
@@ -10,6 +10,9 @@ class KitchenGetOneController extends BaseController
     public function action(string $id): JsonResponse
     {
         $kitchen =  $this->kitchenService->getById($id);
+        if ($kitchen === null) {
+            return Response::SetAndGet(Response::NOT_FOUND, 'Data dapur tidak dapat ditemukan');
+        }
         return Response::SetAndGet(message: 'Dapur Berhasil Didapatkan', data: $kitchen->toArray());
     }
 }
