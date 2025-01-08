ca8649e5e13d291a781cc51d42b3d57322fc3643
randaru09 <ndaruanggoro123@gmail.com>
2025-01-08 17:50:44 +0700

    feat: create update request validation for category

---
+++ app/Http/Requests/Category/UpdateRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Traits\RequestErrorMessage;
+
+class UpdateRequest extends FormRequest
+{
+    use RequestErrorMessage;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'bail|required|string|unique:kitchens|max:255',
+        ];
+    }
+
+    /**
+     * Aliases name
+     * 
+     * @return array
+     */
+    public function attributes(): array
+    {
+        return [
+            'name' => 'Nama',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'unique' => ':attribute sudah digunakan.',
+            'required' => ':attribute wajib diisi.',
+            'string' => ':attribute harus berupa teks.',
+            'max' => ':attribute tidak boleh lebih dari :max karakter.',
+        ];
+    }
+}
