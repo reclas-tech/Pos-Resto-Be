b72ffb4c37891986d28d0b40509470299883973d
randaru09 <ndaruanggoro123@gmail.com>
2025-01-08 17:14:01 +0700

    feat: update controller for kitchen

---
+++ app/Http/Controllers/Kitchen/KitchenUpdateController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Kitchen;
+
+use App\Http\Requests\Kitchen\UpdateRequest;
+use Illuminate\Http\JsonResponse;
+use App\Helpers\Response;
+
+class KitchenUpdateController extends BaseController
+{
+    public function action(UpdateRequest $request, string $id): JsonResponse
+    {
+        $response = new Response(message: 'Edit Dapur Berhasil');
+
+        $kitchen = $this->kitchenService->getById($id);
+
+        if ($kitchen !== null) {
+            
+            [
+                'name' => $name,
+            ] = $request;
+
+            $this->kitchenService->update(
+                kitchen : $kitchen,
+                name: $name,
+            );
+
+        } else {
+            $response->set(Response::NOT_FOUND, 'Data dapur tidak dapat ditemukan');
+        }
+
+        return $response->get();
+    }
+}
