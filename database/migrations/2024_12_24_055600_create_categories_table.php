3a6099d34d97a5ac50352e6cf1ed22e39653ed6e
randaru09 <ndaruanggoro123@gmail.com>
2025-01-13 16:24:22 +0700

    delete comment

--- database/migrations/2024_12_24_055600_create_categories_table.php
+++ database/migrations/2024_12_24_055600_create_categories_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('categories', function (Blueprint $table): void {
             $table->uuid('id')->primary();
 
-            // UNIQUE
             $table->string('name', 255);
 
             $table->timestamps();
