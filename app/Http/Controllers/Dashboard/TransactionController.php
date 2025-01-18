d99bc378ce44f266ae7da8735d5cd00ab141e126
randaru09 <ndaruanggoro123@gmail.com>
2025-01-18 18:36:07 +0700

    feat: transaction controller for dashboard

---
+++ app/Http/Controllers/Dashboard/TransactionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use Illuminate\Http\JsonResponse;
+use App\Helpers\Response;
+
+class TransactionController extends BaseController
+{
+    public function action(): JsonResponse
+    {
+        $income =  $this->dashboardService->transaction();
+        return Response::SetAndGet(message: 'Jumlah Transaksi Berhasil Didapatkan', data: $income->toArray());
+    }
+}
