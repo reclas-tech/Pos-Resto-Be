2ee0b0c10405acc58606997e3f7bdb01528bf370
randaru09 <ndaruanggoro123@gmail.com>
2025-01-25 01:15:56 +0700

    feat: close cashier invoice

---
+++ app/Http/Controllers/CashOnHand/CloseCashierInvoiceController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\CashOnHand;
+
+use Illuminate\Http\JsonResponse;
+use App\Helpers\Response;
+
+class CloseCashierInvoiceController extends BaseController
+{
+    public function action(string $id): JsonResponse
+    {
+
+        $cashon = $this->cashOnHandService->getOne($id);
+
+        if ($cashon === null) {
+            return Response::SetAndGet(Response::NOT_FOUND, 'Data struk tidak dapat ditemukan');
+        }   
+
+        $data = $this->cashOnHandService->closeCashierInvoice($cashon);
+
+        return Response::SetAndGet(message: 'Data struk Berhasil Didapatkan', data:$data );
+    }
+}
--- app/Http/Services/CashOnHand/CashOnHandService.php
+++ app/Http/Services/CashOnHand/CashOnHandService.php
@@ -4,6 +4,9 @@ namespace App\Http\Services\CashOnHand;
 
 use App\Models\CashierShift;
 use App\Models\Employee;
+use App\Models\Invoice;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use App\Http\Services\Service;
 use Exception;
@@ -60,6 +63,67 @@ class CashOnHandService extends Service
 		}
 
 		return $cashon;
+
+	}
+
+	/**
+	 * @param string $id
+	 * 
+	 * @return \App\Models\CashierShift|null
+	 */
+	public function getOne(string $id): CashierShift|null
+	{
+
+		return CashierShift::where('id', $id)->first();
+
+	}
+
+
+	/**
+	 * @param \App\Models\CashierShift $cashon
+	 * 
+	 * @return Collection|\Exception
+	 */
+	public function closeCashierInvoice(CashierShift $cashon): Collection|Exception
+	{
+		$data = collect();
+
+		$date = Carbon::parse($cashon->started_at)->format('Y-m-d');
+
+		$transaction = Invoice::whereDate('created_at', $date)->get();
+
+		$income = $transaction->where('status', Invoice::SUCCESS)->sum('price_sum');
+		$transaction_count =  $transaction->where('status', Invoice::SUCCESS)->count();
+
+		$cash = $transaction->where('status', Invoice::SUCCESS)->where('payment', Invoice::CASH)->sum('price_sum');
+
+		$failed_cash = $transaction->where('status', Invoice::CANCEL)->where('payment', Invoice::CASH)->sum('price_sum');
+		
+		$debit = $transaction->where('status', Invoice::SUCCESS)->where('payment', Invoice::DEBIT)->sum('price_sum');
+
+		$qris = $transaction->where('status', Invoice::SUCCESS)->where('payment', Invoice::QRIS)->sum('price_sum');
+
+		$total = $cash + $debit + $qris;
+
+		$data->add([
+			'date' => $cashon->started_at,
+			'cashier_name' => $cashon->cashier->name,
+			'start_at' => $cashon->started_at,
+			'end_at' => $cashon->ended_at,
+			'cash_on_hand_start' => $cashon->cash_on_hand_start,
+			'income' => $income,
+			'cash_on_hand_end' => $cashon->cash_on_hand_end,
+			'transaction_count' => $transaction_count,
+			'success_inv' => $cash,
+			'failed_inv' => $failed_cash,
+			'out_inv' => $cashon->cash_on_hand_start + $cash,
+			'cash' => $cash,
+			'debit' => $debit,
+			'qris' => $qris,
+			'total' => $total
+		]);
+
+		return $data;
 
 	}
 
